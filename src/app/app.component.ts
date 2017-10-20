ec87d00ebcbd8c2b035901cf665f5b0ea88eede1
Sam <phreakdawg@gmail.com>
2017-10-19 21:14:59 -0600

    Dev and prod mode oauth redirect urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
@@ -7,11 +7,14 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Youtube Playlist Manager';
+export class AppComponent implements OnInit {
+  public title = 'Youtube Playlist Manager';
   public IsAuthenticated: boolean;
+  public oauth_url: string;
+
 
   constructor(private authService: AuthService, private activatedRoute: ActivatedRoute) {
+
     this.activatedRoute.fragment.subscribe((fragment: string) => {
       if (fragment) {
         let qstringParsed = this.parseQueryString(fragment);
@@ -23,6 +26,15 @@ export class AppComponent {
         this.authService.SetAuthenticated(false);
       }
     })
+  }
+
+  public ngOnInit(): void {
+    if (isDevMode()) {
+      this.oauth_url = "https://accounts.google.com/o/oauth2/v2/auth?client_id=769089562908-q38pfih651l28ikrd8b1h5aif80bl1kf.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A4200&response_type=token&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube";
+    }
+    else {
+      this.oauth_url = "https://accounts.google.com/o/oauth2/v2/auth?client_id=769089562908-q38pfih651l28ikrd8b1h5aif80bl1kf.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Fsamhaycraft.net%2FYoutube&response_type=token&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube";
+    }
   }
 
   private parseQueryString(query: string): any {
