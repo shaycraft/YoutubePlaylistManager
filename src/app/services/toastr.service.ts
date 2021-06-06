a2dbe5bd151176182f13482e68cfe823a32d91d0
Sam <phreakdawg@gmail.com>
2021-06-06 03:13:34 -0600

    Chore/toastr tmp fix (#15)
    * does stuff
    
    * yep
    
    * karma update
    
    * I dunno
    
    * Angulat 11 update
    
    * yep
    
    * Observables and shit
    
    * ivy enable
    
    * lint

--- src/app/services/toastr.service.ts
+++ src/app/services/toastr.service.ts
@@ -6,10 +6,13 @@ export class ToastrService {
 
   public manager() {
     return {
-      warn: () => {},
-      info: () => {},
-      success: () => {},
-      error: () => {},
+      /* eslint-disable-next-line */
+      warn: (msg: string) => console.debug(`WARN: ${msg}`),
+      /* eslint-disable-next-line */
+      info: (msg: string) => console.debug(`INFO: ${msg}`),
+      /* eslint-disable-next-line */
+      success: (msg: string) => console.debug(`SUCCESS: ${msg}`),
+      error: (msg: string) => console.error(`ERROR-6000: ${msg}`),
     };
   }
 }
--- src/app/services/youtube-api.service.ts
+++ src/app/services/youtube-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operators';
 
 @Injectable()
@@ -14,9 +13,6 @@ export class YoutubeApiService {
   public getPlaylistItems(id: string, limit: number): Observable<any> {
     const url = `${this.baseUrl}/playlistItems?part=snippet&maxResults=${limit}&playlistId=${id}&key=${this.apiKey}`;
 
-    return this.http.get<any>(url)
-    .pipe(
-      map(response => response.json()),
-    );
+    return this.http.get<any>(url);
   }
 }
