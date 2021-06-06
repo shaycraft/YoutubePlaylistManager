f77974c60b63a54c90f878913867ff262f1005f5
Sam Haycraft <phreakdawg@gmail.com>
2021-06-06 03:07:26 -0600

    lint

--- src/app/services/toastr.service.ts
+++ src/app/services/toastr.service.ts
@@ -6,8 +6,11 @@ export class ToastrService {
 
   public manager() {
     return {
+      /* eslint-disable-next-line */
       warn: (msg: string) => console.debug(`WARN: ${msg}`),
+      /* eslint-disable-next-line */
       info: (msg: string) => console.debug(`INFO: ${msg}`),
+      /* eslint-disable-next-line */
       success: (msg: string) => console.debug(`SUCCESS: ${msg}`),
       error: (msg: string) => console.error(`ERROR-6000: ${msg}`),
     };
