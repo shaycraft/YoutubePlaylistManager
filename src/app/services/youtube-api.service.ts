a419e8230cec4d4b10bcb8b546ead1c973655a91
Sam Haycraft <phreakdawg@gmail.com>
2021-06-06 02:03:25 -0600

    yep

--- src/app/services/youtube-api.service.ts
+++ src/app/services/youtube-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operators';
 
 @Injectable()
